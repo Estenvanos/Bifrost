2fb32a83b29bb9efe9d2c45ac0f9cd1266f8778b
Estevan <peghettino04@gmail.com>
2026-02-10 16:32:47 -0300

    🔨fix/remove the folder dist

--- server/dist/models/Cart.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Cart = void 0;
-const mongoose_1 = require("mongoose");
-const CartSchema = new mongoose_1.Schema({
-    user_id: { type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: true },
-    active: { type: Boolean, default: true },
-}, { timestamps: true });
-exports.Cart = mongoose_1.models.Cart || (0, mongoose_1.model)("Cart", CartSchema);
